25e4f1104ae40a39f5918683bd78f0236678367a
Supereg <mail@anderl-bauer.de>
2020-09-29 19:59:09 +0200

    Adjust tests

--- src/MDNSServer.spec.ts
+++ src/MDNSServer.spec.ts
@@ -3,25 +3,25 @@ import { MDNSServer, SendResultFailedRatio } from "./MDNSServer";
 describe(MDNSServer, () => {
   it("SendResultFailedRatio", () => {
     expect(SendResultFailedRatio([
-      { status: "fulfilled", interface: "eth0", value: undefined},
-      { status: "fulfilled", interface: "eth0", value: undefined},
-      { status: "fulfilled", interface: "eth0", value: undefined},
-      { status: "fulfilled", interface: "eth0", value: undefined},
-      { status: "fulfilled", interface: "eth0", value: undefined},
+      { status: "fulfilled", interface: "eth0"},
+      { status: "fulfilled", interface: "eth0"},
+      { status: "fulfilled", interface: "eth0"},
+      { status: "fulfilled", interface: "eth0"},
+      { status: "fulfilled", interface: "eth0"},
     ])).toBe(0);
 
     expect(SendResultFailedRatio([
       { status: "rejected", interface: "eth0", reason: new Error()},
-      { status: "fulfilled", interface: "eth0", value: undefined},
+      { status: "fulfilled", interface: "eth0"},
       { status: "rejected", interface: "eth0", reason: new Error()},
-      { status: "fulfilled", interface: "eth0", value: undefined},
-      { status: "fulfilled", interface: "eth0", value: undefined},
-    ])).toBe(0.4);
+      { status: "fulfilled", interface: "eth0"},
+      { status: "timeout", interface: "eth0"},
+    ])).toBe(0.6);
 
     expect(SendResultFailedRatio([
       { status: "rejected", interface: "eth0", reason: new Error()},
       { status: "rejected", interface: "eth0", reason: new Error()},
-      { status: "rejected", interface: "eth0", reason: new Error()},
+      { status: "timeout", interface: "eth0"},
       { status: "rejected", interface: "eth0", reason: new Error()},
       { status: "rejected", interface: "eth0", reason: new Error()},
     ])).toBe(1);
